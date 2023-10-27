4d62fb767ed8dffeff8dc8dec646e2a886772c65
Jonathan Guamuch <joelmorales0598@gmail.com>
2023-10-27 13:57:06 -0600

    CATALOGOS PARA MUESTRA MEDICA

--- backend/src/main/java/com/proyecto/progra/backend/model/dao/MuestraDao.java
+++ backend/src/main/java/com/proyecto/progra/backend/model/dao/MuestraDao.java
@@ -3,5 +3,6 @@ package com.proyecto.progra.backend.model.dao;
 import com.proyecto.progra.backend.model.entity.Muestra;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface MuestraDao extends JpaRepository<Muestra, Integer> {
 }
